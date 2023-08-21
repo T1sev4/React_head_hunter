c8f01c7fc7948d31935e80c7d6f28d717436676b
t1sev4 <tamikofficial@gmail.com>
2023-08-21 15:32:42 +0600

    lesson 4 part 5, add useEffect for get array of cities, and also create dropdown with select city

--- src/app/create-resume/page.js
+++ src/app/create-resume/page.js
@@ -1,14 +1,25 @@
 'use client'
 import Header from '../../components/header'
 import Input from '@/components/input'
+import AutoCompleteSelect from '@/components/AutoCompleteSelect'
 import { END_POINT } from '@/config/end-point'
 import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 export default function CreateResume() {
 
+  const [cities, setCities] = useState([]);
 
-  axios.get(`${END_POINT}/api/region/cities`).then(res => {
-    console.log(res);
-  })
+  useEffect(() => {
+   
+    axios.get(`${END_POINT}/api/region/cities`).then(res => {
+      setCities(res.data)
+    })
+  }, [])
+
+  const onSelect = (data) => {
+    console.log("onSelect", data)
+  }
 
 
   return (
@@ -21,7 +32,8 @@ export default function CreateResume() {
         <Input placeholder="" type="text" label="Имя" size="fieldset-md" />
         <Input placeholder="" type="text" label="Фамилия" size="fieldset-md" />
         <Input placeholder="" type="text" label="Мобильный телефон" size="fieldset-md" />
-        <Input placeholder="" type="text" label="Город проживание" size="fieldset-md" />
+        <AutoCompleteSelect placeholder="" type="text" label="Город проживание" size="fieldset-md" items={cities} onSelect={onSelect}/>
+
 
       </div>
     </main>
