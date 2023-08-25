9b2a1a05f81081b866c8ac325be0ec3ad1419c6e
t1sev4 <tamikofficial@gmail.com>
2023-08-26 02:30:39 +0600

    lesson 5 part 1, adding new fieldsets in create resume

--- src/app/create-resume/page.js
+++ src/app/create-resume/page.js
@@ -5,15 +5,19 @@ import AutoCompleteSelect from '@/components/AutoCompleteSelect'
 import { END_POINT } from '@/config/end-point'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-
+import SelectDate from '@/components/SelectDate'
 export default function CreateResume() {
 
   const [cities, setCities] = useState([]);
+  const [countries, setCountries] = useState([]);
 
   useEffect(() => {
    
     axios.get(`${END_POINT}/api/region/cities`).then(res => {
       setCities(res.data)
+    })
+    axios.get(`${END_POINT}/api/region/countries`).then(res => {
+      setCountries(res.data)
     })
   }, [])
 
@@ -34,6 +38,28 @@ export default function CreateResume() {
         <Input placeholder="" type="text" label="Мобильный телефон" size="fieldset-md" />
         <AutoCompleteSelect placeholder="" type="text" label="Город проживание" size="fieldset-md" items={cities} onSelect={onSelect}/>
 
+        <h3>Основная информация</h3>
+
+        <SelectDate size="fieldset-sm" label="Дата рождение" />
+
+
+        <fieldset className="fieldset fieldset-sm">
+          <label>Пол</label>
+          <div className='radio-group'>
+            <div className='radio'>
+            <input  type="radio" name='gender' id='g1' />
+            <label for="g1" >Мужской</label>
+            </div>
+            <div className='radio'>
+            <input type="radio" name='gender' id='g2' />
+            <label for="g2">Женский</label>
+            </div>
+           
+            
+          </div>
+        </fieldset>
+
+        <AutoCompleteSelect placeholder="" type="text" label="Гражданство" size="fieldset-md" items={countries} onSelect={onSelect}/>
 
       </div>
     </main>
